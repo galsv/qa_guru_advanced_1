9bd5324491e283461e7e5c5b4ccd0c2fd9f290be
Sergey Galtsov <galtsovsergey@mail.ru>
2024-07-31 19:10:46 +0300

    app running from docker

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM python:3.12
+
+RUN pip install poetry==1.8.3
+
+ENV POETRY_NO_INTERACTION=1 \
+    POETRY_VIRTUALENVS_IN_PROJECT=1 \
+    POETRY_VIRTUALENVS_CREATE=1 \
+    POETRY_CACHE_DIR=/tmp/poetry_cache
+
+WORKDIR /code
+COPY pyproject.toml poetry.lock  /code/
+
+RUN poetry install && rm -rf $POETRY_CACHE_DIR
+
+COPY ./app /code/app
+
+CMD ["poetry", "run", "fastapi", "run", "app/main.py", "--port", "80"]
+
+# If running behind a proxy like Nginx or Traefik add --proxy-headers
+# CMD ["fastapi", "run", "app/main.py", "--port", "80", "--proxy-headers"]
--- app/main.py
+++ app/main.py
@@ -1,18 +1,27 @@
 import dotenv
 import uvicorn
+import logging
 from fastapi import FastAPI
-
-from routers import status, users
+from contextlib import asynccontextmanager
+from app.routers import status, users
 from app.database.engine import create_db_and_tables
 from fastapi_pagination import add_pagination
 
 dotenv.load_dotenv()
+
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    logging.warning("On startup")
+    create_db_and_tables()
+    yield
+    logging.warning("On shutdown")
+
 app = FastAPI()
+add_pagination(app)
 app.include_router(status.router)
 app.include_router(users.router)
 
 
 if __name__ == "__main__":
-    create_db_and_tables()
-    add_pagination(app)
     uvicorn.run(app, host="localhost", port=8002)
